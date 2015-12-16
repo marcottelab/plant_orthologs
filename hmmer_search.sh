9f4cc423a74dcdda9cbc8cbc0a60224fcc9005dc
cmpalmer15 <cmpalmer15@utexas.edu>
2015-12-15 19:12:16 -0600

    delete hmmer_search.sh
    moved

--- hmmer_search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# $1: directory where hmms are stored
-# $2: fasta file containing proteome 
-# $3: directory to save output to
-
-cat $1/*.hmm > $3/hmms
-hmmpress $3/hmms
-hmmsearch --tblout $3/search_results.tsv $3/hmms $2
