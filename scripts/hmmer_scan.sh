87c3eeb9a0590af64096cc8b8ef1bfa68ea334c8
Claire Palmer <cp341279@ada.icmb.utexas.edu>
2015-12-17 12:09:54 -0600

    Now works with ortho_tabscan.sh and provides option to use an existing hmmdatabase.

--- scripts/hmmer_scan.sh
+++ scripts/hmmer_scan.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#hmmer_scan.sh
+# $1: fasta file containing proteome
+# $2: path and name of directory to store results
+# $3: name for hmm database, if already created include path
+# optional $4: directory where hmms are stored if haven't made hmm database yet
 
-cat minihmm/*.hmm > minidata
-hmmpress minidata
-hmmscan --tblout  scan_results.tsv  minidata miniproteome.fasta
-
+if [[ $# -eq 4 ]]; then
+	cat $4/*.hmm > $2/$3
+	hmmpress $2/$3
+	hmmscan -o $2/"scan_results.txt" $2/$3 $1
+else
+	hmmscan -o $2/"scan_results.txt" $3 $1
+fi
