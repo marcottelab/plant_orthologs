7ba53e22d7e67987e81eee16fbd36c058c6b19d9
Claire Palmer <cp341279@ada.icmb.utexas.edu>
2016-01-11 13:44:26 -0600

    Changed so now can specify results name (for analyzing proteome in chunks)

--- scripts/hmmer_scan.sh
+++ scripts/hmmer_scan.sh
@@ -3,12 +3,13 @@
 # $1: fasta file containing proteome
 # $2: path and name of directory to store results
 # $3: name for hmm database, if already created include path
-# optional $4: directory where hmms are stored if haven't made hmm database yet
+# $4: name for scan results file
+# optional $5: directory where hmms are stored if haven't made hmm database yet
 
-if [[ $# -eq 4 ]]; then
-	cat $4/*.hmm > $2/$3
+if [[ $# -eq 5 ]]; then
+	echo $5/*.hmm | xargs cat > $2/$3
 	hmmpress $2/$3
-	hmmscan -o $2/"scan_results.txt" $2/$3 $1
+	hmmscan -o $2/$4 $2/$3 $1
 else
-	hmmscan -o $2/"scan_results.txt" $3 $1
+	hmmscan -o $2/$4 $3 $1
 fi
