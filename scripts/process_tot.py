760e0946b23a995eb410fda667a07032559674d6
Claire Palmer <cp341279@ada.icmb.utexas.edu>
2016-02-11 18:13:27 -0600

    Added QueryRange column to process_tot.py

--- scripts/process_tot.py
+++ scripts/process_tot.py
@@ -3,7 +3,7 @@
 # argv[2]: name and path of hmmerscan results
 # argv[3]: level of hmms scanned against (ie eukaryotes)
 # argv[4]: name and path for output (can make new file or add to existing)
-# Returns a space seperated text file with rank, level, proteinID, orthogroupID, evalue and proteomeID (from proteome file name). Rank 0 orthogroup is proteinID when a protein has no significant hits (evalue>0.01). All hits with an e-value<=0.01 yield an entry, rank is based on order in hmmer results.
+# Returns a space seperated text file with rank, level, proteinID, orthogroupID, evalue, QueryRange, and proteomeID (from proteome file name). Rank 0 orthogroup is proteinID when a protein has no significant hits (evalue>0.01). All hits with an e-value<=0.01 yield an entry, rank is based on order in hmmer results.
 
 import os.path
 import sys
@@ -40,7 +40,10 @@ def process_tot():
 				OGid = protein[i].id.split('.')
 				OGid = OGid[0]+"."+OGid[1]+" "
 				e = str(protein[i].evalue)+" "
-				processed.append((rank, level, pid, OGid, e, omeid))
+				qr = [] #empty list for domain ranges of this hit
+				for d in protein[i]:
+					qr.append(d.query_range)
+				processed.append((rank, level, pid, OGid, e, str(qr)+" ", omeid))
 				i += 1
 
 	#write entries to file
@@ -48,7 +51,7 @@ def process_tot():
 		output = open(sys.argv[4], "a")
 	else: #else make a new file and add a header
 		output = open(sys.argv[4], "w")
-		output.write("Rank Level ProteinID GroupID evalue ProteomeID \n")
+		output.write("Rank Level ProteinID GroupID evalue QueryRange ProteomeID \n")
 	for i in processed:
 		output.write("".join(str(s) for s in i) + "\n")
 	output.close()	
